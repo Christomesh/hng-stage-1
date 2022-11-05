f0bd02faf17eaf1d95fda542ab9cc6f3a379c116
Meshach Odejide <meshacholuwadamilare@gmail.com>
2022-11-05 23:29:51 +0100

    hng-stage2

--- main.go
+++ main.go
@@ -1,42 +1,93 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"net/http"
 )
 
-type jsonResponse struct {
-	SlackUsername string `json:"slackusername"`
+type firstJson struct {
+	SlackUsername string `json:"slackUsername"`
 	Backend       bool   `json:"backend"`
 	Age           int    `json:"age"`
 	Bio           string `json:"bio"`
 }
 
+type operationExpectedJson struct {
+	OperationType string `json:"operation_type" validate:"required,eq=addition|eq=subtraction|eq=multiplication"`
+	X             int64  `json:"x"`
+	Y             int64  `json:"y"`
+}
+type operationJsonResponse struct {
+	SlackUsername string `json:"slackUsername"`
+	OperationType string `json:"operation_type" validate:"required,eq=addition|eq=subtraction|eq=multiplication"`
+	Result        int64  `json:"result"`
+}
+
 func main() {
 	port := "3000"
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", giveResponse)
-	fmt.Println("Server listening on port: " + port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))
+	r := gin.New()
+	//r.GET("/", giveResponse())
+	r.POST("/", performOperation())
+	r.Run(":" + port)
 
 	//.Println("Server listening on port: " + port)
 }
 
-func giveResponse(w http.ResponseWriter, r *http.Request) {
-	myRes := &jsonResponse{
-		SlackUsername: "Meshach",
-		Backend:       true,
-		Age:           22,
-		Bio:           "A junior backend developer",
-	}
-	resp, err := json.Marshal(myRes)
-	if err != nil {
-		log.Println(err)
-	}
-	w.Header().Set("Content-Type", "application/json")
+func performOperation() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		//var operation operationExpectedJson
+		operation := &operationExpectedJson{
+			OperationType: "addition",
+			X:             8,
+			Y:             8,
+		}
+		var operationResp operationJsonResponse
+		var validate = validator.New()
+		if err := c.BindJSON(&operation); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		if validationErr := validate.Struct(operation); validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+		operationResp.SlackUsername = "Meshach"
 
-	fmt.Fprintln(w, string(resp))
+		switch operation.OperationType {
+		case "addition":
+			result := operation.X + operation.Y
+			operationResp.Result = result
+			operationResp.OperationType = operation.OperationType
+		case "subtraction":
+			result := operation.X - operation.Y
+			operationResp.Result = result
+			operationResp.OperationType = operation.OperationType
+		case "multiplication":
+			result := operation.X * operation.Y
+			operationResp.Result = result
+			operationResp.OperationType = operation.OperationType
+		default:
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Operation_type is unknown"})
+		}
+		c.JSON(http.StatusOK, operationResp)
 
+	}
+}
+
+func giveResponse() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		myRes := &firstJson{
+			SlackUsername: "Meshach",
+			Backend:       true,
+			Age:           22,
+			Bio:           "A junior backend developer",
+		}
+		//if err := c.BindJSON(&myRes); err != nil {
+		//	c.JSON(http.StatusBadRequest, gin.H{"err": err.Error()})
+		//	return
+		//}
+		c.JSON(http.StatusOK, myRes)
+
+	}
 }
